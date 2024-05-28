3cc1cc2d26a69763a8322aaa28c906b2e2bed4ad
bhaskar <bhaskar.devyanitechnologies@gmail.com>
2024-05-28 21:24:49 +0530

    code deploy

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -2,7 +2,7 @@ const appConfig = {
     TOKENSECRET: "codefortomorrow",
     TOKENEXPIRY: 2592000, // 1 month
     REFRESHTOKENEXPIRY: 86400, // 24 hr
-    REDISPORT: 6397,
+    REDISPORT: 6397, 
     APPCLIENTID:'fifdoubfojsdfbiusadhfijasdfipjasdfiusdafs',
 };
 export default appConfig
